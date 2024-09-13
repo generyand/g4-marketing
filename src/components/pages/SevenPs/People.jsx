59edb0a430291bdeb969e20d32f2c23eccc113dd
generyand <generyan.dep@gmail.com>
2024-09-13 09:43:06 +0800

    added a logo

--- src/components/pages/SevenPs/People.jsx
+++ src/components/pages/SevenPs/People.jsx
@@ -6,7 +6,22 @@ const People = () => {
   return (
     <SubTopicContainer>
       <SubTopicTitle>5. People</SubTopicTitle>
-      <p>This page will contain information about the role of people in marketing.</p>
+      <p className="mb-4 md:mb-8">
+        In the production of a product, the people needed to do the tasks are
+        not directly dealing with the customers so that customer service
+        training is not a priority, but production-related training is.{" "}
+      </p>
+      <p className="mb-4 md:mb-8">
+        People element is vitally important in the service marketing mix. When a
+        service is being delivered, the person delivering it is not unique from
+        the service itself.
+      </p>
+      <p>
+        When dining at a restaurant, if a rude waiter is encountered, the entire
+        experience will be labeled as bad service. This is why many businesses
+        invest in defining the right kind of a person to fill their service role
+        and then making efforts to find or train people to fit this definition.
+      </p>
     </SubTopicContainer>
   );
 };
--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <Link to={"/"} className="flex-shrink-0">
-      <h1 className="text-2xl font-semibold dark:text-white">
-        GROUP<span className="text-blue-400">4</span>
-      </h1>
+      <div className="flex items-center justify-center gap-2">
+        <img
+          src="/next-gen-logo.svg"
+          alt="Next Gen Logo"
+          className="w-8 aspect-square"
+        />
+        <h1 className="text-xl font-semibold sm:text-2xl dark:text-white">
+          GROUP<span className="text-blue-400">4</span>
+        </h1>
+      </div>
     </Link>
   );
 }
