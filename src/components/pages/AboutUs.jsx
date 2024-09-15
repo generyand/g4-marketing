548d2b4c5442d4c9264ed7cac86f135735f90c66
generyand <generyan.dep@gmail.com>
2024-09-15 17:17:00 +0800

    Added technologies to About Us page.

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { FaReact } from "react-icons/fa";
+import { SiFramer, SiFigma } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
 import SubTopicContainer from "../ui/SubTopicContainer";
 import SubTopicTitle from "../ui/SubTopicTitle";
 
@@ -36,6 +39,32 @@ const TEAM_MEMBERS = [
   },
 ];
 
+const TECHNOLOGIES = [
+  {
+    name: "ReactJS",
+    description: "A powerful JavaScript library for building user interfaces",
+    icon: FaReact,
+  },
+  {
+    name: "TailwindCSS",
+    description: "A utility-first CSS framework for rapid UI development",
+    icon: RiTailwindCssFill,
+  },
+  {
+    name: "Framer Motion",
+    description:
+      "A production-ready motion library for React to create smooth animations",
+    icon: SiFramer,
+  },
+  // { name: 'Lottie', description: 'A library for adding high-quality animations to web and mobile projects', icon: SiLottie },
+  {
+    name: "Figma",
+    description:
+      "A collaborative interface design tool used for creating the website layout",
+    icon: SiFigma,
+  },
+];
+
 const TeamMember = ({ name, role, contribution }) => (
   <motion.div
     initial={{ opacity: 0, y: 20 }}
@@ -54,6 +83,26 @@ const TeamMember = ({ name, role, contribution }) => (
     <p className="text-sm text-gray-600 sm:text-base dark:text-gray-400">
       {contribution}
     </p>
+  </motion.div>
+);
+
+const TechnologyCard = ({ name, description, icon: Icon }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 20 }}
+    transition={{ duration: 0.5 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true }}
+    className="p-4 bg-white rounded-lg shadow-md md:p-4 dark:bg-gray-800"
+  >
+    <div className="flex flex-col items-center text-center">
+      <div className="p-4 mb-4 bg-blue-100 rounded-full dark:bg-blue-900">
+        <Icon className="w-8 h-8 text-blue-500 dark:text-blue-300" />
+      </div>
+      <h4 className="mb-1 font-semibold text-gray-800 dark:text-gray-200">
+        {name}
+      </h4>
+      <p className="text-xs text-gray-700 dark:text-gray-300">{description}</p>
+    </div>
   </motion.div>
 );
 
@@ -87,9 +136,34 @@ function AboutUs() {
         >
           Our Team
         </motion.h3>
-        <div className="grid grid-cols-1 gap-2 sm:gap-4 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 mb-12 sm:grid-cols-2">
           {TEAM_MEMBERS.map((member, index) => (
             <TeamMember key={index} {...member} />
+          ))}
+        </div>
+
+        <motion.h3
+          initial={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="mt-12 mb-1 text-xl font-semibold text-gray-800 dark:text-gray-200"
+        >
+          Technologies
+        </motion.h3>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="mb-4 text-gray-700 md:mb-6 dark:text-gray-300 sm:text-lg"
+        >
+          Our website is built using cutting-edge technologies to ensure a
+          smooth, interactive, and visually appealing user experience:
+        </motion.p>
+        <div className="grid grid-cols-2 gap-2 md:gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          {TECHNOLOGIES.map((tech, index) => (
+            <TechnologyCard key={index} {...tech} />
           ))}
         </div>
       </div>
