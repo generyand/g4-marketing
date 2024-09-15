ae7e06fd27a098fb6c6ffd1ee3c669c99d8006f8
generyand <generyan.dep@gmail.com>
2024-09-15 18:41:49 +0800

    updated searchData

--- src/data/searchData.js
+++ src/data/searchData.js
@@ -1,11 +1,43 @@
 export const searchData = [
   { title: "Key Terminologies", path: "/key-terminologies" },
-  { title: "Basic Terms", path: "/key-terminologies/basic-terms" },
-  { title: "7P's of Marketing", path: "/7ps-of-marketing" },
+  {
+    title: "Marketing Strategy Business Plan",
+    path: "/key-terminologies/marketing-strategy-business-plan",
+  },
+  { title: "Target Market", path: "/key-terminologies/target-market" },
+  {
+    title: "Competitor Analysis",
+    path: "/key-terminologies/competitor-analysis",
+  },
+  { title: "Company Overview", path: "/key-terminologies/company-overview" },
+  {
+    title: "Product/Service Plan",
+    path: "/key-terminologies/product-service-plan",
+  },
+  { title: "Marketing Plan", path: "/key-terminologies/marketing-plan" },
+  { title: "The 7P's of Marketing", path: "/7ps-of-marketing" },
   { title: "Product", path: "/7ps-of-marketing/product" },
-  { title: "Price", path: "/7ps-of-marketing/price" },
+  { title: "Pricing", path: "/7ps-of-marketing/pricing" },
+  { title: "Place", path: "/7ps-of-marketing/place" },
+  { title: "Promotion", path: "/7ps-of-marketing/promotion" },
+  { title: "People", path: "/7ps-of-marketing/people" },
+  { title: "Process", path: "/7ps-of-marketing/process" },
+  { title: "Physical Evidence", path: "/7ps-of-marketing/physical-evidence" },
   { title: "Branding", path: "/branding" },
-  { title: "Brand Identity", path: "/branding/brand-identity" },
+  { title: "History of Branding", path: "/branding/history" },
+  { title: "Definition", path: "/branding/definition" },
+  {
+    title: "Characteristics of a Good Brand",
+    path: "/branding/characteristics",
+  },
   { title: "Marketing Research", path: "/marketing-research" },
-  { title: "Market Analysis", path: "/marketing-research/market-analysis" },
+  {
+    title: "Reasons to Conduct",
+    path: "/marketing-research/reasons-to-conduct",
+  },
+  { title: "Guide to Conduct", path: "/marketing-research/guide-to-conduct" },
+  {
+    title: "Need and Importance",
+    path: "/marketing-research/need-and-importance",
+  },
 ];
