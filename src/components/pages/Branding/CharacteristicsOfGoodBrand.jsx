6debefe81ce3b62f629c59d2b5ccc023ce163e96
generyand <generyan.dep@gmail.com>
2024-09-15 11:22:41 +0800

    minor change

--- src/components/pages/Branding/CharacteristicsOfGoodBrand.jsx
+++ src/components/pages/Branding/CharacteristicsOfGoodBrand.jsx
@@ -6,7 +6,7 @@ const CharacteristicsOfGoodBrand = () => {
   return (
     <SubTopicContainer>
       <SubTopicTitle>Characteristics of a Good Brand</SubTopicTitle>
-      <p className="mb-4 md:mb-8">This page outlines the key characteristics that make up a strong and effective brand.</p>
+      {/* <p className="mb-4 md:mb-8">This page outlines the key characteristics that make up a strong and effective brand.</p> */}
       <div className="mb-8 overflow-hidden text-xs rounded-lg md:text-base md:mb-12">
         <table className="min-w-full overflow-hidden bg-gray-300 border-collapse table-auto">
           <thead>
