52ad0c99ce681a1449e0893af95afcbdfa019bdf
generyand <generyan.dep@gmail.com>
2024-09-15 10:27:05 +0800

    removed next gen logo from header

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -5,11 +5,11 @@ function Logo() {
   return (
     <Link to={"/"} className="flex-shrink-0">
       <div className="flex items-center justify-center gap-2">
-        <img
+        {/* <img
           src="/next-gen-logo.svg"
           alt="Next Gen Logo"
           className="w-8 aspect-square"
-        />
+        /> */}
         <h1 className="text-xl font-semibold sm:text-2xl dark:text-white">
           GROUP<span className="text-blue-400">4</span>
         </h1>
