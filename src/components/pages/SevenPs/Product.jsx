cb5553fd3977003be7d65954abf6cb4fbea747dc
generyand <generyan.dep@gmail.com>
2024-09-17 13:34:10 +0800

    minor changes

--- src/components/pages/SevenPs/Product.jsx
+++ src/components/pages/SevenPs/Product.jsx
@@ -13,41 +13,41 @@ function Product() {
         consumption of the product are simultaneous, and the product is
         intangible, diverse, and perishable.
       </p>
-      <div className="mb-8 overflow-hidden text-xs text-center rounded-lg md:text-base md:mb-12">
-        <table className="min-w-full overflow-hidden bg-gray-300 border-collapse table-auto">
+      <div className="overflow-hidden mb-8 text-xs text-center rounded-lg md:text-base md:mb-12">
+        <table className="overflow-hidden min-w-full bg-gray-300 border-collapse table-auto">
           <thead>
             <tr className="text-white bg-blue-500">
-              <th className="p-2 border-b border-r border-white md:p-4 last:border-r-0">
+              <th className="p-2 border-r border-b border-white md:p-4 last:border-r-0">
                 Characteristics
               </th>
-              <th className="p-2 border-b border-r border-white md:p-4 last:border-r-0">
+              <th className="p-2 border-r border-b border-white md:p-4 last:border-r-0">
                 Manufacturing Industry
               </th>
-              <th className="p-2 border-b border-r border-white md:p-4 last:border-r-0">
+              <th className="p-2 border-r border-b border-white md:p-4 last:border-r-0">
                 Service Industry
               </th>
             </tr>
           </thead>
           <tbody className="bg-blue-100 dark:text-gray-800">
             <tr>
-              <td className="p-2 border-b border-r border-white md:p-4">
+              <td className="p-2 border-r border-b border-white md:p-4">
                 Production and Consumption
               </td>
-              <td className="p-2 border-b border-r border-white md:p-4">Not Simultaneous</td>
+              <td className="p-2 border-r border-b border-white md:p-4">Not Simultaneous</td>
               <td className="p-2 border-b border-white md:p-4">Simultaneous</td>
             </tr>
             <tr>
-              <td className="p-2 border-b border-r border-white md:p-4">
+              <td className="p-2 border-r border-b border-white md:p-4">
                 Product Tangibility{" "}
               </td>
-              <td className="p-2 border-b border-r border-white md:p-4">Tangible</td>
+              <td className="p-2 border-r border-b border-white md:p-4">Tangible</td>
               <td className="p-2 border-b border-white md:p-4">Intangible</td>
             </tr>
             <tr>
-              <td className="p-2 border-b border-r border-white md:p-4">
+              <td className="p-2 border-r border-b border-white md:p-4">
                 Product Diversity
               </td>
-              <td className="p-2 border-b border-r border-white md:p-4">
+              <td className="p-2 border-r border-b border-white md:p-4">
                 Not always diverse
               </td>
               <td className="p-2 border-b border-white md:p-4">Diverse</td>
@@ -60,11 +60,11 @@ function Product() {
           </tbody>
         </table>
       </div>
-      <p>
+      {/* <p>
         The nature of this "product" allows for on the spot customization. This
         also means that the point at which this activity is occurring becomes
         very important.
-      </p>
+      </p> */}
       
     </SubTopicContainer>
   );
